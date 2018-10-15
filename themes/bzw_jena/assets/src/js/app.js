a3e77e5e077eaf4705380a3721cffe570f956c7c
mlfct <webmaster@byte-revolution.com>
2018-10-15 17:43:52 +0200

    Layout fuer Praxisunterseiten

--- themes/bzw_jena/assets/src/js/app.js
+++ themes/bzw_jena/assets/src/js/app.js
@@ -13,6 +13,17 @@ $(document).ready(function(){
     once: true
   });
 
+  // TYPEKIT LOADER
+
+  (function(d) {
+    var config = {
+        kitId: 'hms3yeh',
+        scriptTimeout: 3000,
+        async: true
+      },
+      h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+
   // FB Shariff
   var buttonsContainer = $('.some-selector');
   new Shariff(buttonsContainer, {
